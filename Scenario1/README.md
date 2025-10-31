b9d50e47adf365c2c070d02621741a71f2ce40fe
Alaa CHAAR <alaa.chaar@skf.com>
2025-10-31 12:08:31 +0100

    fastMCP with stdio

---
+++ Scenario1/README.md
@@ -0,0 +1,2 @@
+## Scenario 1 : FastMCP with stdio 
+* npx @modelcontextprotocol/inspector python3 fastmcp_calculator.py : pour tester le serveur 
--- Scenario1/fastmcp_calculator.py
+++ Scenario1/fastmcp_calculator.py
@@ -56,4 +56,4 @@ def divide(a: float, b: float) -> float:
     return a / b
 
 if __name__ == "__main__":
-    mcp.run() #STDIN/STDOUT interface par default
+    mcp.run() #STDIO interface par default
