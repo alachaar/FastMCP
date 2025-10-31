6522ec669a4adddf31f9804ff20ac33af86d21d4
Alaa CHAAR <alaa.chaar@skf.com>
2025-10-31 14:20:03 +0100

    Only FastMCP with http streamable

--- Scenario1/README.md
+++ Scenario1/README.md
@@ -1,2 +1,8 @@
-## Scenario 1 : FastMCP with stdio 
+## Scenario 1 :
+
+# FastMCP with stdio 
 * npx @modelcontextprotocol/inspector python3 fastmcp_calculator.py : pour tester le serveur 
+
+# FastMCP with streamable http 
+* python3 fastmcp_calculatorV2.py : lancer le serveur
+* npx @modelcontextprotocol/inspector python3 http://localhost:8003/mcp : pour tester le serveur 
---
+++ Scenario1/fastmcp_calculatorV2.py
@@ -0,0 +1,59 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP(name="Calculator")
+
+@mcp.tool()
+def multiply(a: float, b: float) -> float:
+    """Multiplies two numbers.
+
+    args: a (float): The first number.
+            b (float): The second number.
+    
+    returns: float: The product of the two numbers.
+    """
+    return a * b
+
+
+@mcp.tool(
+    name="add",
+    description="Adds two numbers.",
+    tags=["math", "addition"],
+)
+def add_numbers(x: float, y: float) -> float:
+    """Adds two numbers.
+
+    args: x (float): The first number.
+            y (float): The second number.
+    
+    returns: float: The sum of the two numbers.
+    """
+    return x + y
+
+
+@mcp.tool()
+def subtract(a: float, b: float) -> float:
+    """Subtracts the second number from the first number.
+
+    args: a (float): The first number.
+            b (float): The second number.
+    
+    returns: float: The difference of the two numbers.
+    """
+    return a - b
+
+
+@mcp.tool()
+def divide(a: float, b: float) -> float:
+    """Divides the first number by the second number.
+
+    args: a (float): The numerator.
+            b (float): The denominator.
+    
+    returns: float: The quotient of the two numbers.
+    """
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
+
+if __name__ == "__main__":
+    mcp.run(transport="http", host="localhost", port=8003) #HTTP interface 
